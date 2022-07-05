94bc762d627319cf398d8604352957d2fdb2ea1a
Edward Nunez <edwardnunez31@gmail.com>
2022-07-05 10:36:52 -0400

    get add

--- server.js
+++ server.js
@@ -20,13 +20,22 @@ let db,
         db =  client.db(dbName)
         collection = db.collection('movies')
     })
-//middleware
+//middleware (
 app.set('view engine', 'ejs')//adding ejs to our to machine
 app.use(express.static('public'))//setting some external files , serve up all the files from the public folder
 app.use(express.urlencoded({extended:true}))//helps us halnded url 
 app.use(express.json())
 app.use(cors())
-//middleware
+//middleware )
+
+app.get('/',async (req, res) => {
+    try{
+        res.render('index.ejs')
+    } catch(error) {
+        res.status(500).send({message: error.message})
+    }
+})
+
 
 //connect it to the port (this is using express)
 app.listen(process.env.PORT || PORT, () => {
